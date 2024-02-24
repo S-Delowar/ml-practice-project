3d8dbe175af42ad92ac6e0694fc0a72ea377d794
S-Delowar <sayed.buet97@gmail.com>
2024-02-24 16:18:13 +0600

    update logger and exception

--- src/exception.py
+++ src/exception.py
@@ -14,11 +14,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-
-if __name__ == "__main__":
-    try:
-        a = 2 / 0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e, sys)
-    
