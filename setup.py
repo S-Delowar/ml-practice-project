53b0b2b938769de4642de5153ffdb44d4f77655b
S-Delowar <sayed.buet97@gmail.com>
2024-03-03 07:54:31 +0600

    update setup

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
-from setuptools import setup, find_packages
-from typing import List
+# from setuptools import setup, find_packages
+# from typing import List
 
-def get_requirements(file_to_path:str)-> List[str]:
-    """This function creates the list of requirements
+# def get_requirements(file_to_path:str)-> List[str]:
+#     """This function creates the list of requirements
 
-    Args:
-        file_to_path (str): library names
+#     Args:
+#         file_to_path (str): library names
 
-    Returns:
-        List[str]: list of the library names
-    """
-    requirements = []
-    HYPHEN_E_DOT = "-e ."
+#     Returns:
+#         List[str]: list of the library names
+#     """
+#     requirements = []
+#     HYPHEN_E_DOT = "-e ."
     
-    with open(file_to_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
+#     with open(file_to_path) as file_obj:
+#         requirements = file_obj.readlines()
+#         requirements = [req.replace("\n", "") for req in requirements]
         
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)    
-    return requirements
+#         if HYPHEN_E_DOT in requirements:
+#             requirements.remove(HYPHEN_E_DOT)    
+#     return requirements
         
         
-setup(
-    name = "mlproject",
-    version= "0.0.1",
-    author= "sayed_delwoar",
-    author_email= "sahed.delowar@gmail.com",
-    packages= find_packages(),
-    install_requires= get_requirements('requirements.txt')
+# setup(
+#     name = "mlproject",
+#     version= "0.0.1",
+#     author= "sayed_delwoar",
+#     author_email= "sahed.delowar@gmail.com",
+#     packages= find_packages(),
+#     install_requires= get_requirements('requirements.txt')
     
-)
+# )
